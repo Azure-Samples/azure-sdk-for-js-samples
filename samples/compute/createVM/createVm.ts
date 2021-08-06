570f481f508b70962533093a69e43a6f1de94a79
qiaozha <qiaozha@microsoft.com>
2021-08-06 10:27:25 +0800

    update samples according to arch board review suggestions

--- samples/compute/createVM/createVm.ts
+++ samples/compute/createVM/createVm.ts
@@ -1,139 +1,134 @@
-import * as compute from "@azure/arm-compute";
-import * as network from "@azure/arm-network";
-import * as resources from "@azure/arm-resources";
-import {DefaultAzureCredential} from "@azure/identity";
+import { ComputeManagementClient, VirtualMachine } from "@azure/arm-compute";
+import { NetworkManagementClient, VirtualNetwork, Subnet, NetworkInterface } from "@azure/arm-network";
+import { ResourceManagementClient, ResourceGroup } from "@azure/arm-resources";
+import {DefaultAzureCredential } from "@azure/identity";
 
-class TestCreateVirtualMachines {
+const subscriptionId = "92f95d8f-3c67-4124-91c7-8cf07cdbf241";
+const resourceGroupName = "qiaozhatest1";
+const virtualMachineName = "virtualmachinex";
+const subnetName = "subnetnamex";
+const interfaceName = "interfacex";
+const networkName = "networknamex";
+const location = "eastus";
+let computeClient: ComputeManagementClient;
+let networkClient: NetworkManagementClient;
+let resourcesClient: ResourceManagementClient;
 
-    private computeClient;
-    private networkClient;
-    private resourcesClient;
-    private credential;
-
-    private subscriptionId = "92f95d8f-3c67-4124-91c7-8cf07cdbf241";
-    private resourceGroupName = "qiaozhatest1";
-    private virtualMachineName = "virtualmachinex";
-    private subnetName = "subnetnamex";
-    private interfaceName = "interfacex";
-    private networkName = "networknamex";
-    private location = "eastus";
-
-    constructor() {
-        this.credential = new DefaultAzureCredential();
-        this.computeClient = new compute.ComputeManagementClient(this.credential, this.subscriptionId);
-        this.networkClient = new network.NetworkManagementClient(this.credential, this.subscriptionId);
-        this.resourcesClient = new resources.ResourceManagementClient(this.credential, this.subscriptionId);
-    }
-    //virtualMachines.createOrUpdate
-    public async createVirtualMachines() {
-        await this.createResourceGroup();
-        await this.createVirtualNetwork();
-        await this.createNetworkInterface(this.resourceGroupName, this.location, this.interfaceName);
-        const parameter: compute.VirtualMachine = {
-            location: this.location,
-            hardwareProfile: {
-                vmSize: "Standard_D2_v2",
+//virtualMachines.createOrUpdate
+async function createVirtualMachines() {
+    await createResourceGroup();
+    await createVirtualNetwork();
+    await createNetworkInterface(resourceGroupName, location, interfaceName);
+    const parameter: VirtualMachine = {
+        location: location,
+        hardwareProfile: {
+            vmSize: "Standard_D2_v2",
+        },
+        storageProfile: {
+            imageReference: {
+                sku: "2016-Datacenter",
+                publisher: "MicrosoftWindowsServer",
+                version: "latest",
+                offer: "WindowsServer"
             },
-            storageProfile: {
-                imageReference: {
-                    sku: "2016-Datacenter",
-                    publisher: "MicrosoftWindowsServer",
-                    version: "latest",
-                    offer: "WindowsServer"
+            osDisk: {
+                caching: "ReadWrite",
+                managedDisk: {
+                    storageAccountType: "Standard_LRS"
                 },
-                osDisk: {
-                    caching: "ReadWrite",
-                    managedDisk: {
-                        storageAccountType: "Standard_LRS"
-                    },
-                    name: "myVMosdisk",
-                    createOption: "FromImage"
-                },
-                dataDisks: [
-                    {
-                        diskSizeGB: 1023,
-                        createOption: "Empty",
-                        lun: 0
-                    },
-                    {
-                        diskSizeGB: 1023,
-                        createOption: "Empty",
-                        lun: 1
-                    }
-                ]
+                name: "myVMosdisk",
+                createOption: "FromImage"
             },
-            osProfile: {
-                adminUsername: "testuser",
-                computerName: "myVM",
-                adminPassword: "Aa1!zyx_",
-                windowsConfiguration: {
-                    enableAutomaticUpdates: true // need automatic update for reimage
-                }
-            },
-            networkProfile: {
-                networkInterfaces: [
-                    {
-                        id: "/subscriptions/" + this.subscriptionId + "/resourceGroups/" + this.resourceGroupName + "/providers/Microsoft.Network/networkInterfaces/" + this.interfaceName + "",
-                        primary: true
-                    }
-                ]
-            }
-        };
-        const res = await this.computeClient.virtualMachines.beginCreateOrUpdateAndWait(this.resourceGroupName, this.virtualMachineName, parameter);
-        console.log(res);
-    }
-
-    private async createResourceGroup() {
-        const parameter: resources.ResourceGroup = {
-            location: "eastus",
-            tags: {
-                tag1: "value1"
-            }
-        };
-        await this.resourcesClient.resourceGroups.createOrUpdate(this.resourceGroupName, parameter).then(
-            result => {
-                console.log(result);
-            }
-        )
-    }
-
-    // networkClient.virtualNetworks.createOrUpdate
-    private async createVirtualNetwork() {
-        const parameter: network.VirtualNetwork = {
-            location: this.location,
-            addressSpace: {
-                addressPrefixes: ['10.0.0.0/16']
-            }
-        };
-        const virtualNetworks_create_info = await this.networkClient.virtualNetworks.beginCreateOrUpdateAndWait(this.resourceGroupName, this.networkName, parameter);
-        console.log(virtualNetworks_create_info);
-
-        const subnet_parameter: network.Subnet = {
-            addressPrefix: "10.0.0.0/24"
-        };
-        const subnet_create_info = await this.networkClient.subnets.beginCreateOrUpdateAndWait(this.resourceGroupName, this.networkName, this.subnetName, subnet_parameter);
-        console.log(subnet_create_info)
-    }
-
-    // networkClient.networkInterfaces.createOrUpdate
-    private async createNetworkInterface(group_name: any, location: any, nic_name: any) {
-        const parameter: network.NetworkInterface = {
-            location: location,
-            ipConfigurations: [
+            dataDisks: [
                 {
-                    name: "MyIpConfig",
-                    subnet: {
-                        id: "/subscriptions/" + this.subscriptionId + "/resourceGroups/" + this.resourceGroupName + "/providers/Microsoft.Network/virtualNetworks/" + this.networkName + "/subnets/" + this.subnetName
-
-                    }
+                    diskSizeGB: 1023,
+                    createOption: "Empty",
+                    lun: 0
+                },
+                {
+                    diskSizeGB: 1023,
+                    createOption: "Empty",
+                    lun: 1
                 }
             ]
-        };
-        const nic_info = await this.networkClient.networkInterfaces.beginCreateOrUpdateAndWait(group_name, nic_name, parameter);
-        console.log(nic_info);
-    }
-
+        },
+        osProfile: {
+            adminUsername: "testuser",
+            computerName: "myVM",
+            adminPassword: "Aa1!zyx_",
+            windowsConfiguration: {
+                enableAutomaticUpdates: true // need automatic update for reimage
+            }
+        },
+        networkProfile: {
+            networkInterfaces: [
+                {
+                    id: "/subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroupName + "/providers/Microsoft.Network/networkInterfaces/" + interfaceName + "",
+                    primary: true
+                }
+            ]
+        }
+    };
+    const res = await computeClient.virtualMachines.beginCreateOrUpdateAndWait(resourceGroupName, virtualMachineName, parameter);
+    console.log(res);
 }
 
-const t = new TestCreateVirtualMachines();
-t.createVirtualMachines();
+// networkClient.networkInterfaces.createOrUpdate
+async function createNetworkInterface(group_name: any, location: any, nic_name: any) {
+    const parameter: NetworkInterface = {
+        location: location,
+        ipConfigurations: [
+            {
+                name: "MyIpConfig",
+                subnet: {
+                    id: "/subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroupName + "/providers/Microsoft.Network/virtualNetworks/" + networkName + "/subnets/" + subnetName
+
+                }
+            }
+        ]
+    };
+    const nic_info = await networkClient.networkInterfaces.beginCreateOrUpdateAndWait(group_name, nic_name, parameter);
+    console.log(nic_info);
+}
+
+async function createResourceGroup() {
+    const parameter: ResourceGroup = {
+        location: "eastus",
+        tags: {
+            tag1: "value1"
+        }
+    };
+    await resourcesClient.resourceGroups.createOrUpdate(resourceGroupName, parameter).then(
+        result => {
+            console.log(result);
+        }
+    )
+}
+
+// networkClient.virtualNetworks.createOrUpdate
+async function createVirtualNetwork() {
+    const parameter: VirtualNetwork = {
+        location: location,
+        addressSpace: {
+            addressPrefixes: ['10.0.0.0/16']
+        }
+    };
+    const virtualNetworks_create_info = await networkClient.virtualNetworks.beginCreateOrUpdateAndWait(resourceGroupName, networkName, parameter);
+    console.log(virtualNetworks_create_info);
+
+    const subnet_parameter: Subnet = {
+        addressPrefix: "10.0.0.0/24"
+    };
+    const subnet_create_info = await networkClient.subnets.beginCreateOrUpdateAndWait(resourceGroupName, networkName, subnetName, subnet_parameter);
+    console.log(subnet_create_info)
+}
+
+async function main() {
+    const credential = new DefaultAzureCredential();
+    computeClient = new ComputeManagementClient(credential, subscriptionId);
+    networkClient = new NetworkManagementClient(credential, subscriptionId);
+    resourcesClient = new ResourceManagementClient(credential, subscriptionId);
+    await createVirtualMachines();
+}
+
+main();
